b1345299e000a00aef29815350afd73b24adb69d
Marco Crespo <macrespo305@gmail.com>
2022-12-08 20:26:25 -0500

    1st commit

---
+++ src/components/DriverCard.js
@@ -0,0 +1,17 @@
+import Rating from "./Rating";
+
+const DriverCard = (props) => {
+
+    let {name, rating, img, car} = props
+
+    return (
+        <>
+        <h3>{name}</h3>
+        <Rating>{rating}</Rating>
+        <img src={img} alt="profilepic"/>
+        <h3>{car.model} {car.licensePlate}</h3>
+        </>
+    )
+}
+
+export default DriverCard;
